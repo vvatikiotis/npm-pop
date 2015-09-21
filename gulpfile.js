98e2cb4b6bf5d9e52eb24348f5de69ad33e2d3a8
Vassilis Vatikiotis <vvatikiotis@gmail.com>
2015-09-21 23:53:59 +0300

    gulp: additional tasks (build task is broken)

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 var gulp = require('gulp'),
+    del = require('del'),
+    plumber = require('gulp-plumber'),
+    shell = require('gulp-shell'),
     PATHS = {
         src: {
             html: 'client/**/*.html',
@@ -6,7 +9,21 @@ var gulp = require('gulp'),
         }
     };
 
-gulp.task('default', function() {
+gulp.task('clean', function() {
+    return del(['dist']);
+});
+
+gulp.task('build', ['clean'], function() {
+    return gulp.src(PATHS.src.js)
+        // .pipe(plumber())
+        .pipe(shell([
+            'jspm bundle-sfx index.js dist/index.js',
+            './node_modules/.bin/uglifyjs dist/index.js -o dist/app.min.js',
+            './node_modules/.bin/html-dist client/index.html --remove-all --minify --insert app.min.js -o dist/index.html'
+        ]));
+});
+
+gulp.task('dev-watch', function() {
     var browserSync = require('browser-sync'),
         reload = browserSync.reload;
 
@@ -19,3 +36,5 @@ gulp.task('default', function() {
     gulp.watch(PATHS.src.html).on('change', reload);
     gulp.watch(PATHS.src.js).on('change', reload);
 });
+
+gulp.task('default', ['dev-watch']);
