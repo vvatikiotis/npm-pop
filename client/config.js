df6d0576e6ed3c8a6374d036ffe4244dfe515b38
Vassilis Vatikiotis <vvatikiotis@gmail.com>
2015-09-23 15:05:41 +0300

    feat: basic prototype, search for a package and get the monthly stats.

--- client/config.js
+++ client/config.js
@@ -14,17 +14,34 @@ System.config({
   },
 
   map: {
+    "axios": "npm:axios@0.6.0",
     "babel": "npm:babel-core@5.8.25",
     "babel-runtime": "npm:babel-runtime@5.8.24",
+    "bootstrap": "github:twbs/bootstrap@3.3.5",
     "core-js": "npm:core-js@1.1.4",
+    "css": "github:systemjs/plugin-css@0.1.17",
     "react": "npm:react@0.13.3",
     "react-router": "npm:react-router@1.0.0-rc1",
     "underscore": "npm:underscore@1.8.3",
+    "github:jspm/nodelibs-assert@0.1.0": {
+      "assert": "npm:assert@1.3.0"
+    },
     "github:jspm/nodelibs-buffer@0.1.0": {
       "buffer": "npm:buffer@3.5.0"
     },
     "github:jspm/nodelibs-events@0.1.1": {
       "events": "npm:events@1.0.2"
+    },
+    "github:jspm/nodelibs-http@1.7.1": {
+      "Base64": "npm:Base64@0.2.1",
+      "events": "github:jspm/nodelibs-events@0.1.1",
+      "inherits": "npm:inherits@2.0.1",
+      "stream": "github:jspm/nodelibs-stream@0.1.0",
+      "url": "github:jspm/nodelibs-url@0.1.0",
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "github:jspm/nodelibs-https@0.1.0": {
+      "https-browserify": "npm:https-browserify@0.0.0"
     },
     "github:jspm/nodelibs-path@0.1.0": {
       "path-browserify": "npm:path-browserify@0.0.0"
@@ -35,14 +52,33 @@ System.config({
     "github:jspm/nodelibs-stream@0.1.0": {
       "stream-browserify": "npm:stream-browserify@1.0.0"
     },
+    "github:jspm/nodelibs-url@0.1.0": {
+      "url": "npm:url@0.10.3"
+    },
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
+    },
+    "github:twbs/bootstrap@3.3.5": {
+      "jquery": "github:components/jquery@2.1.4"
     },
     "npm:amdefine@1.0.0": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "module": "github:jspm/nodelibs-module@0.1.0",
       "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:assert@1.3.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:axios@0.6.0": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "http": "github:jspm/nodelibs-http@1.7.1",
+      "https": "github:jspm/nodelibs-https@0.1.0",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0",
+      "url": "github:jspm/nodelibs-url@0.1.0"
     },
     "npm:babel-runtime@5.8.24": {
       "process": "github:jspm/nodelibs-process@0.1.1"
@@ -75,6 +111,9 @@ System.config({
       "qs": "npm:qs@4.0.0",
       "warning": "npm:warning@2.0.0"
     },
+    "npm:https-browserify@0.0.0": {
+      "http": "github:jspm/nodelibs-http@1.7.1"
+    },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
@@ -101,6 +140,9 @@ System.config({
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
     "npm:path-browserify@0.0.0": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:punycode@1.3.2": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:react-router@1.0.0-rc1": {
@@ -140,6 +182,12 @@ System.config({
     "npm:through@2.3.8": {
       "process": "github:jspm/nodelibs-process@0.1.1",
       "stream": "github:jspm/nodelibs-stream@0.1.0"
+    },
+    "npm:url@0.10.3": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0",
+      "punycode": "npm:punycode@1.3.2",
+      "querystring": "npm:querystring@0.2.0",
+      "util": "github:jspm/nodelibs-util@0.1.0"
     },
     "npm:util@0.10.3": {
       "inherits": "npm:inherits@2.0.1",
--- client/index.js
+++ client/index.js
@@ -1,14 +1,117 @@
 import React from 'react';
 
-var App = React.createClass({
+/* TODO how do we include css in JSPM? Already installed jspm css plugin */
+import "bootstrap/css/bootstrap.css!";
+
+/* NOTE request-promise npm package fails with "tls module not supported in browsrs", so we use axios */
+import axios from 'axios';
+
+var Search = React.createClass({
+    npmStatsURL: "https://api.npmjs.org/downloads/point",
+
+    retrievePackageStats: function(packageName) {
+        axios.get(this.npmStatsURL + '/last-month/' + packageName)
+            .then((response) => {
+                console.dir(response.data);
+                this.props.onResults(response.data);
+            })
+    },
+    handleSubmit: function(evt) {
+            evt.preventDefault();
+            var packageName = React.findDOMNode(this.refs.package).value.trim();
+            if (!packageName) return;
+            this.retrievePackageStats(packageName);
+    },
     render: function() {
         return (
-            <div>Hello</div>
+            <div className="row">
+                <div className="col-sm-12">
+            <form className="form-horizontal"
+                onSubmit={this.handleSubmit}
+            >
+                <div className="form-group">
+                    <label className="col-sm-2 control-label"
+                        htmlFor="packageName"
+                    >Search package</label>
+                    <div className="col-sm-10">
+                        <input className="form-control"
+                            id="packageName"
+                            placeholder="Search package..."
+                            ref="package"
+                            type="text"
+                        />
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+        );
+    }
+});
+
+
+var PackageInfo = React.createClass({
+    render: function() {
+
+        var obj = Object.keys(this.props.data).map((key) => {
+            return (<div><strong>{key}</strong>: {this.props.data[key]}</div>);
+        });
+        console.dir(obj);
+        return (
+            <div>{obj}</div>
+        );
+    }
+});
+
+var Results = React.createClass({
+    render: function() {
+        return (
+            <div className="row">
+                <div className="col-sm-12">
+            <div className="panel panel-default">
+                <div className="panel-body">
+                    <PackageInfo data={this.props.packageInfo} />
+                </div>
+            </div>
+        </div>
+    </div>
+        );
+    }
+});
+
+
+
+var PACKAGE_RESULTS = {
+    downloads: 31241234,
+    package: "request",
+    start: "2015-09-21",
+    end: "2015-09-21"
+};
+
+var App = React.createClass({
+    getInitialState: function() {
+        return {
+            searchResults: ''
+        };
+    },
+    onResults: function(searchResults) {
+        this.setState({
+            searchResults: searchResults
+        });
+    },
+    render: function() {
+        return (
+            <div className="container">
+                <Search
+                    onResults={this.onResults} />
+                <Results
+                    packageInfo={this.state.searchResults} />
+            </div>
         )
     }
 });
 
 React.render(
     <App />,
-    document.getElementById('app')
+    document.getElementById('root')
 );
